1908a4f9007e044c67afbf8b746eac26d2fdfc35
Sam McCallum <sgmccallum@icloud.com>
2024-12-11 09:16:43 +0000

    n repeats experiments

--- experiments/odes/run_checkpointed.sh
+++ experiments/odes/run_checkpointed.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-checkpoints=(2 4 8 16 32 64)
-for n in "${checkpoints[@]}"
-do
-    python3 lotka_volterra.py --checkpoints "$n"
+checkpoints=(2 4 8 16 32 44)
+repeats=4
+for i in $(seq 1 $repeats); do
+  echo "Iteration $i"
+  for n in "${checkpoints[@]}"
+    do
+        python3 pendulum.py --checkpoints "$n"
+    done
 done
